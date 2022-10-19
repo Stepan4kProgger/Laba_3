09e211c49704229d31afc944136f0578a46120d7
Stepan4kProgger <104154921+Stepan4kProgger@users.noreply.github.com>
2022-10-19 14:04:59 +0300

    Delete MaterialUnit.cpp

--- MaterialUnit.cpp
+++
@@ -1,8 +0,0 @@
-#include "MaterialUnit.h"
-using namespace std;
-
-MaterialUnit::MaterialUnit() { cout << '|'; }
-void MaterialUnit::setName(string name) { this->name = name; }
-void MaterialUnit::setPrice(double price) { this->price = price; }
-string MaterialUnit::getName() { return name; }
-double MaterialUnit::getPrice() { return price; }
